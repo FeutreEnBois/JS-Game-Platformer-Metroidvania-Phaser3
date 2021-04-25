6e94e7b75b5d2ad14ff2eefa49f3c3484db6c3b3
FeutreEnBois <derosier.maxime@gmail.com>
2021-04-25 18:30:59 +0200

    this

--- newOne/Level1.js
+++ newOne/Level1.js
@@ -1,37 +1,37 @@
-var worldLayer;
-var belowLayer;
-var aboveLayer;
-var deathLayer;
-var player;
-var cursors;
-var playerSpeed = 150;
-var jumpTimer = 0;
-var platforms;
-var keyZ;
-var keyQ;
-var keyS;
-var keyD;
-var keyP;
-var keyO;
-var keyI;
-var nbrDash = 2;
-var dash = 4;
-var delayBTWDash = 60;
-var delay = 0;
-var cooldownDash = false;
-var volume = 0.4;
-var music;
-var controls;
-var enemy1;
-var enemy2;
-var enemy3;
-var enemy4;
-var goblin;
-var Doors
-var endX;
-var endY;
+// var worldLayer;
+// var belowLayer;
+// var aboveLayer;
+// var deathLayer;
+// var player;
+// var cursors;
+// var playerSpeed = 150;
+// var jumpTimer = 0;
+// // var platforms;
+// // var keyZ;
+// // var keyQ;
+// // var keyS;
+// // var keyD;
+// // var keyP;
+// // var keyO;
+// // var keyI;
+// // var nbrDash = 2;
+// // var dash = 4;
+// // var delayBTWDash = 60;
+// // var delay = 0;
+// // var cooldownDash = false;
+// // var volume = 0.4;
+// // var music;
+// // var controls;
+// // var enemy1;
+// // var enemy2;
+// // var enemy3;
+// // var enemy4;
+// // var goblin;
+// // var Doors
+// // var endX;
+// // var endY;
 var bullets;
-var Enemies;
+// var Enemies;
 
 class Level1 extends Phaser.Scene {
     constructor() {
@@ -42,106 +42,102 @@ class Level1 extends Phaser.Scene {
 
         // config music
         this.sound.stopAll();
-        music = this.sound.add('luna');
-        music.setVolume(0.5);
-        music.setLoop(true);
-        music.play();
+        this.music = this.sound.add('luna');
+        this.music.setVolume(0.5);
+        this.music.setLoop(true);
+        this.music.play();
 
         // get map
         const map = this.make.tilemap({ key: "intro" });
         const tileset = map.addTilesetImage("oubliette", "tileset");
-        worldLayer = map.createStaticLayer("Background", tileset, 0, 0)
-        deathLayer = map.createStaticLayer("death", tileset, 0, 0)
-        belowLayer = map.createStaticLayer("fond", tileset, 0, 0)
-        aboveLayer = map.createStaticLayer("sol", tileset, 0, 0)
+        this.worldLayer = map.createStaticLayer("Background", tileset, 0, 0)
+        this.deathLayer = map.createStaticLayer("death", tileset, 0, 0)
+        this.belowLayer = map.createStaticLayer("fond", tileset, 0, 0)
+        this.aboveLayer = map.createStaticLayer("sol", tileset, 0, 0)
 
         // set collides
-        belowLayer.setCollision([0, 5]);
-        this.flames = deathLayer.setCollisionByProperty({ death : true});
-        platforms = aboveLayer.setCollisionByProperty({ collides: true });
+        this.belowLayer.setCollision([0, 5]);
+        this.flames = this.deathLayer.setCollisionByProperty({ death : true});
+        this.platforms = this.aboveLayer.setCollisionByProperty({ collides: true });
 
         // config keyboard input
-        cursors = this.input.keyboard.createCursorKeys();
+        this.cursors = this.input.keyboard.createCursorKeys();
 
-        Enemies = this.add.group();
+        this.Enemies = this.add.group();
 
         // set doors(for cross level)
-        Doors = this.add.group();
+        this.Doors = this.add.group();
         map.findObject('Objects', function (object) {
             if (object.name === "finishPoint") {
                 endX = object.x
                 endY = object.y
             }
         })
-        var end = this.add.rectangle(endX + 8, endY, 16, 16, 0x5c5a5a, 128)
-        this.physics.add.group(end);
+        this.end = this.add.rectangle(endX + 8, endY, 16, 16, 0x5c5a5a, 128)
+        this.physics.add.group(this.end);
 
         // create some enemy
         map.createFromObjects('goblin', "goblin", {}).forEach((object) => 
-        { Enemies.add(new Goblin(this, object.x, object.y)); object.destroy(); });
+        { this.Enemies.add(new Goblin(this, object.x, object.y)); object.destroy(); });
 
         // create and config player
-        player = new Player(this, 32, 32);
-        this.physics.add.overlap(player, Enemies, () => { player.player_get_hit() }, null, this);
-        this.physics.add.collider(platforms, Enemies);
-        this.physics.add.collider(player, platforms);
-        this.physics.add.collider(player, this.flames, () => this.scene.restart())
-        player.body.setCollideWorldBounds(true);
+        this.player = new Player(this, 32, 32);
+        this.physics.add.overlap(this.player, this.Enemies, () => { this.player.player_get_hit() }, null, this);
+        this.physics.add.collider(this.platforms, this.Enemies);
+        this.physics.add.collider(this.player, this.platforms);
+        this.physics.add.collider(this.player, this.flames, () => this.scene.restart())
+        this.player.body.setCollideWorldBounds(true);
 
 
         // set the keyboard input
-        keyZ = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.Z);
-        keyS = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
-        keyD = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
-        keyQ = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.Q);
-        keyP = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.P);
-        keyO = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.O);
-        keyI = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.I);
+        this.keyP = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.P);
+        this.keyO = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.O);
+        this.keyI = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.I);
 
         // go next level
-        this.physics.add.collider(end, platforms);
-        this.physics.add.collider(end, player, () => this.scene.start("Level3"));
+        this.physics.add.collider(this.end, this.platforms);
+        this.physics.add.collider(this.end, this.player, () => this.scene.start("Level3"));
 
         // config bullet
         bullets = this.physics.add.group();
-        this.physics.add.overlap(player, bullets, () => { player.player_get_hit() }, null, this);
+        this.physics.add.overlap(this.player, bullets, () => { this.player.player_get_hit() }, null, this);
     }
 
     update() {
 
         // P for pause the physics
-        if (keyP.isDown) {
+        if (this.keyP.isDown) {
             this.physics.pause();
-            music.pause();
+            this.music.pause();
         }
 
         // O for resume the physics
-        else if (keyO.isDown) {
+        else if (this.keyO.isDown) {
             this.physics.resume();
-            music.resume();
+            this.music.resume();
         }
         
         // I for restart current scene
-        else if (keyI.isDown) {
-            music.stop();
+        else if (this.keyI.isDown) {
+            this.music.stop();
             this.scene.restart(); 
 
         }
 
         // player moovements
-        player.update()
+        this.player.update()
 
         // enemy moovements
-        Enemies.getChildren().forEach((enemy) => { 
+        this.Enemies.getChildren().forEach((enemy) => { 
             enemy.update();
-            var dist = Phaser.Math.Distance.BetweenPoints(player, enemy);
+            var dist = Phaser.Math.Distance.BetweenPoints(this.player, enemy);
             if (dist < 200) {
                 // enemy go and shoot left(-) or right(+) if player is left or right of the enemy
-                if (player.x < enemy.x) {
+                if (this.player.x < enemy.x) {
                     enemy.body.velocity.x = -40;
                     enemy.shoot2G()
                 }
-                else if (player.x > enemy.x) {
+                else if (this.player.x > enemy.x) {
                     enemy.body.velocity.x = 40;
                     enemy.shoot2D()
                 }
@@ -150,15 +146,5 @@ class Level1 extends Phaser.Scene {
                 enemy.body.velocity.x = 0;
             }
          });
-
-        if (keyZ.isDown) {
-            console.log('Z key pressed')
-        } else if (keyS.isDown) {
-            console.log('S key pressed')
-        } else if (keyD.isDown) {
-            console.log('D key pressed')
-        } else if (keyQ.isDown) {
-            console.log('Q key pressed')
-        }
     }
 }
--- newOne/level4.js
+++ newOne/level4.js
@@ -49,73 +49,73 @@ class Level4 extends Phaser.Scene {
         const map = this.make.tilemap({ key: "lvl4" });
 
         // get map
-        let tileset = map.addTilesetImage("oubliette", "tileset");
-        worldLayer = map.createStaticLayer("Background", tileset, 0, 0)
-        belowLayer = map.createStaticLayer("fond", tileset, 0, 0)
-        aboveLayer = map.createStaticLayer("sol", tileset, 0, 0)
+        const tileset = map.addTilesetImage("oubliette", "tileset");
+        this.worldLayer = map.createStaticLayer("Background", tileset, 0, 0)
+        this.belowLayer = map.createStaticLayer("fond", tileset, 0, 0)
+        this.aboveLayer = map.createStaticLayer("sol", tileset, 0, 0)
 
         // set collides
-        belowLayer.setCollision([0, 5]);
-        platforms = aboveLayer.setCollisionByProperty({ collides: true });
+        this.belowLayer.setCollision([0, 5]);
+        this.platforms = this.aboveLayer.setCollisionByProperty({ collides: true });
 
         // config keyboard input
-        cursors = this.input.keyboard.createCursorKeys();
+        this.cursors = this.input.keyboard.createCursorKeys();
 
         // set doors(for cross level)
-        Doors = this.add.group(); 
+        this.Doors = this.add.group(); 
         map.findObject('Objects', function(object){
             if (object.name === "finishPoint"){
                 endX = object.x
                 endY = object.y
             }
         })
-        var end = this.add.rectangle(endX+8, endY, 16, 16, 0x5c5a5a, 128)
-        this.physics.add.group(end);
-        this.physics.add.collider(end, platforms);
+        this.end = this.add.rectangle(endX+8, endY, 16, 16, 0x5c5a5a, 128)
+        this.physics.add.group(this.end);
+        this.physics.add.collider(this.end, this.platforms);
 
         // create and config player
-        player = new Player(this, 100, 210);
-        this.physics.add.collider(player, platforms);
-        player.body.setCollideWorldBounds(true);
+        this.player = new Player(this, 100, 210);
+        this.physics.add.collider(this.player, this.platforms);
+        this.player.body.setCollideWorldBounds(true);
 
         // set the keyboard input
-        keyP = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.P);
-        keyO = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.O);
-        keyI = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.I);
+        this.keyP = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.P);
+        this.keyO = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.O);
+        this.keyI = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.I);
  
         // config boss
-        bosses = this.add.group()
+        this.bosses = this.add.group()
         this.boss = new Boss(this,435,190)
-        bosses.add(this.boss)
-        this.physics.add.overlap(this.boss.player_attack, player, (object, attack) => { this.boss.attack_hit(attack, object) }, null, this);
-        this.physics.add.collider(this.boss, platforms);
+        this.bosses.add(this.boss)
+        this.physics.add.overlap(this.boss.player_attack, this.player, (object, attack) => { this.boss.attack_hit(attack, object) }, null, this);
+        this.physics.add.collider(this.boss, this.platforms);
     }
 
     update() {
 
         // P for pause
-        if (keyP.isDown) {
+        if (this.keyP.isDown) {
             this.physics.pause();
             this.music.pause();
         } 
         // O for resume
-        else if (keyO.isDown){
+        else if (this.keyO.isDown){
             this.physics.resume();
             this.music.resume();
         }
         // I for restart current scene
-        else if (keyI.isDown){
+        else if (this.keyI.isDown){
             // music.stop();
             this.scene.restart();
             this.music.stop();
         }
     
         // player moovements
-        player.update()
+        this.player.update()
 
         // boss moovements
-        bosses.getChildren().forEach((enemy) => { 
-            enemy.distance = player.body.x - enemy.body.x;
+        this.bosses.getChildren().forEach((enemy) => { 
+            enemy.distance = this.player.body.x - enemy.body.x;
             enemy.direction = enemy.distance > 0 ? 1 : -1;
             enemy.update();
          });
